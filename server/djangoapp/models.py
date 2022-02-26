193cf16095d7a6927ec484ffda9b37b9f4722332
Dylane-JC <56173407+Dylane-JC@users.noreply.github.com>
2022-02-26 19:30:35 +0100

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,98 +1,72 @@
-from django.db import models
-from django.utils.timezone import now
+import requests
+import json
+from .models import CarDealer
+from requests.auth import HTTPBasicAuth
 
-# Create your models here.
+# Create a `get_request` to make HTTP GET requests
+# e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
+#                                     auth=HTTPBasicAuth('apikey', api_key))
+def get_request(url, **kwargs):
+    print(kwargs)
+    print("GET from {} ".format(url))
+    try:
+        response = requests.get(url, headers={'Content-Type' : 'application/json'}, params=kwargs)
+    except:
+        # If any error occurs
+        print("Network exception occuered")
+    status_code = response.status_code
+    print("With status{} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-class CarMake(models.Model):
-    ids = models.AutoField(primary_key=True)
-# - Name
-    name = models.CharField(max_length=25)
-# - Description
-    description = models.CharField(max_length=25)
-# - Any other fields you would like to include in car make model
-    year = models.DateField(max_length=6)
-    color = models.CharField(max_length=25)
-# - __str__ method to print a car make object
-    def __str__(self):
-        return self.name
+def get_dealers_from_cf(url, **kwargs):
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url)
+    if json_result:
+        # Get the row list in JSON as dealers
+        dealers = json_result["docs"]
+        # For each dealer object
+        for dealer in dealers:
+            # Get its content in `doc` object
+            dealer_doc = dealer
+            # Create a CarDealer object with values in `doc` object
+            dealer_obj = CarDealer(address=dealer_doc["address"], 
+                                   city=dealer_doc["city"], 
+                                   full_name=dealer_doc["full_name"],
+                                   id=dealer_doc["id"], 
+                                   lat=dealer_doc["lat"], 
+                                   long=dealer_doc["long"],
+                                   short_name=dealer_doc["short_name"],
+                                   st=dealer_doc["st"], 
+                                   zip=dealer_doc["zip"], 
+                                   state=dealer_doc["state"])
+            results.append(dealer_obj)
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-class CarModel(models.Model):
-    ids = models.AutoField(primary_key=True)
-# - Name
-    name = models.CharField(max_length=25)
-# Foreign key
-    carmake = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-# - Dealer id, used to refer a dealer created in cloudant database
-    id_dealer = models.IntegerField()
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-    list_choice = [
-        (1, ('Sedan')), 
-        (2, ('SUV')), 
-        (3, ('WAGON'))
-        ]
-    types = models.CharField(max_length=30, null=True)
-# - Year (DateField)
-    year = models.DateField()
-# - Any other fields you would like to include in car model
-    seler = models.CharField(max_length=25)
-# - __str__ method to print a car make object
-    def __str__(self):
-        return self.name
+    return results
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
-class CarDealer:
-    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip, state):
-        # Dealer address
-        self.address = address
-        # Dealer city
-        self.city = city
-        # Dealer Full Name
-        self.full_name = full_name
-        # Dealer id
-        self.id = id
-        # Location lat
-        self.lat = lat
-        # Location long
-        self.long = long
-        # Dealer short name
-        self.short_name = short_name
-        # Dealer state
-        self.st = st
-        # Dealer zip
-        self.zip = zip
-        # Delaer state
-        self.state = state
-    def __str__(self):
-        return "Dealer name : " + self.full_name
+def get_dealer_reviews_from_cf(url, **kwargs):
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url, dealerId=id)
+    if json_result:
+        # Get the row list in JSON as dealers
+        reviews = json_result["doc"]
+        # For each dealer object
+        for review in reviews:
+            # Get its content in `doc` object
+            review_doc = review
+            # Create a CarDealer object with values in `doc` object
+            review_obj = DealerReview(dealership=review_doc["dealership"], 
+                                      name=review_doc["name"], 
+                                      purchase=review_doc["purchase"],
+                                      review=review_doc["review"], 
+                                      purchase_date=review_doc["purchase_date"], 
+                                      car_make=review_doc["car_make"],
+                                      car_model=review_doc["car_model"], 
+                                      car_year=review_doc["car_year"], 
+                                      sentiment=review_doc["sentiment"], 
+                                      id=review_doc["id"])
+            results.append(review_obj)
         
-# <HINT> Create a plain Python class `DealerReview` to hold review data
-class DealerReview:
-    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
-        self.dealership = dealership
-        # Review name
-        self.name = name
-        # Review purchase
-        self.purchase = purchase
-        # Review content
-        self.review = review
-        # Review purchase date
-        self.purchase_date = purchase_date
-        # Car make
-        self.car_make = car_make
-        # Car model
-        self.car_model = car_model
-        # Car year
-        self.car_year = car_year
-        # Seniment
-        self.sentiment = sentiment
-        # Review id
-        self.id = id
-
-
-
-if __name__ == "__main__":
-    pass
+    return result
