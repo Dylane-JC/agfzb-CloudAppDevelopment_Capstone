17ad58a3849ba703eec7f56378ac5d462cf2aac0
Dylane-JC <56173407+Dylane-JC@users.noreply.github.com>
2022-05-23 23:37:43 +0100

    Update settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -13,8 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(file).resolve().parent.parent
-BASE_DIR = BASE_DIR/server
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
